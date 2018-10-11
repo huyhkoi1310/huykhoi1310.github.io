d18fc1bc62334619bf6918510b05f9ee79d1966e
huyhkoi1310 <41934952+huyhkoi1310@users.noreply.github.com>
2018-10-11 09:32:10 +0700

    bt - máy tính

--- Phần 11...Javascript HTML DOM/DOM Event/js/script.js
+++ Phần 11...Javascript HTML DOM/DOM Event/js/script.js
@@ -1,1 +1,10 @@
-script.js
+function tinhCanBacHai(a){
+	var s='';
+	s=s+a;
+	console.log(s);
+	const display = document.getElementById('x');
+	display.value+=s;
+	var tg=s.slice(1);
+	var kq=Math.sqrt(Number(tg));
+	display.value=s;
+}
