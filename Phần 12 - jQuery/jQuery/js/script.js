9bfd19b9ae84010e6f62a4b60d102d995544b027
huyhkoi1310 <41934952+huyhkoi1310@users.noreply.github.com>
2018-10-16 22:11:54 +0700

    up jQuery

---
+++ Phần 12 - jQuery/jQuery/js/script.js
@@ -0,0 +1,26 @@
+
+$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_moi');
+	$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_new');
+	$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_new1');
+	$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_new2');
+	$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_new3');
+	$('#casio').click(function(event) {
+	/* Act on the event */
+	$('.wrap').addClass('bg_new4');
+});;
+});;
+});;
+});;
+});;
+});;
+
